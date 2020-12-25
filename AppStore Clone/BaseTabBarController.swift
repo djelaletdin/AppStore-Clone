2a787fa7b46eb83cbcd9ffbca33262ba56fa81f6
djelaletdin <jelaletdinov@gmail.com>
2020-12-26 07:42:36 +0900

    TabBar included

---
+++ AppStore Clone/BaseTabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  BaseTabBarController.swift
+//  AppStore Clone
+//
+//  Created by Didar Jelaletdinov on 26.12.2020.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "TODAY", image: #imageLiteral(resourceName: "today_icon")),
+            createNavController(viewController: UIViewController(), title: "APPS", image: #imageLiteral(resourceName: "apps")),
+            createNavController(viewController: UIViewController(), title: "SEARCH", image: #imageLiteral(resourceName: "search"))
+        ]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, image: UIImage)-> UIViewController{
+        
+        viewController.view.backgroundColor = .white
+        viewController.navigationItem.title = title
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        navController.navigationBar.prefersLargeTitles = true
+        return navController
+    }
+}
