7f1878c3a13d360101cba4bd7a5f17ebc6005a0d
djelaletdin <jelaletdinov@gmail.com>
2021-01-10 10:13:53 +0900

    auto resizing for details page

--- AppStore Clone/Controller/Apps/Details/AppDetailController.swift
+++ AppStore Clone/Controller/Apps/Details/AppDetailController.swift
@@ -17,17 +17,23 @@ class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayou
             let urlString = "https://itunes.apple.com/lookup?id=\(appId ?? "")"
             print(urlString)
             Service.shared.fetchGenericJSONData(urlString: urlString) { (result: SearchResult?, error) in
-                print(error)
-                print(result?.results.count)
+                let app = result?.results.first
+                self.app = app
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
             }
         }
     }
+    
+    var app: Result?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .white
         collectionView.register(AppDetailCell.self, forCellWithReuseIdentifier: cellId)
-        navigationController?.isNavigationBarHidden = true
+//        navigationController?.isNavigationBarHidden = true
+        navigationController?.navigationBar.prefersLargeTitles = false
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -36,11 +42,16 @@ class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayou
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! AppDetailCell
-        
+        cell.app = app
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width, height: 300)
+        let dummyCell = AppDetailCell(frame: .init(x: 0, y: 0, width: view.frame.width, height: 1000))
+        dummyCell.app = app
+        dummyCell.layoutIfNeeded()
+        
+        let estimatedSize = dummyCell.systemLayoutSizeFitting(.init(width: view.frame.width, height: 1000))
+        return .init(width: view.frame.width, height: estimatedSize.height)
     }
 }
---
+++ AppStore Clone/Views/Apps/Details/PreviewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PreviewCell.swift
+//  AppStore Clone
+//
+//  Created by Didar Jelaletdinov on 10.01.2021.
+//
+
+import UIKit
+
+class PreviewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
