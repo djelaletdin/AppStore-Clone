60f6f3e646701c4c73d3f2fd3fc61cb51299236c
djelaletdin <jelaletdinov@gmail.com>
2021-01-26 00:24:25 +0900

    search bar bug fixed

--- AppStore Clone/Model/SearchResult.swift
+++ AppStore Clone/Model/SearchResult.swift
@@ -18,8 +18,8 @@ struct Result: Decodable {
     var averageUserRating: Float?
     let screenshotUrls: [String]
     let artworkUrl100: String
-    let formattedPrice: String
+    var formattedPrice: String?
     let description: String
-    let releaseNotes: String
+    var releaseNotes: String?
 
 }
