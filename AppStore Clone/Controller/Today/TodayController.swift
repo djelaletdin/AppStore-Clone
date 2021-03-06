e839d42e9de90b384fa7bcf177924acd695b94b3
djelaletdin <jelaletdinov@gmail.com>
2021-03-07 00:07:17 +0900

    home page animation added (not fully implemented yet)

--- AppStore Clone/Controller/Today/TodayController.swift
+++ AppStore Clone/Controller/Today/TodayController.swift
@@ -9,61 +9,79 @@ import UIKit
 
 class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     
-    fileprivate let cellId = "todaycell"
+    fileprivate let cellId = "cellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
-        collectionView.register(TodayCell.self, forCellWithReuseIdentifier: cellId)
         
+        navigationController?.isNavigationBarHidden = true
+        
+        collectionView.backgroundColor = #colorLiteral(red: 0.948936522, green: 0.9490727782, blue: 0.9489068389, alpha: 1)
+        
+        collectionView.register(TodayCell.self, forCellWithReuseIdentifier: cellId)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: false)
-    }
+    var appFullscreenController: UIViewController!
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let redview = UIView()
-        redview.backgroundColor = .red
-        redview.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleRemoveRedView(gesture:))))
-        view.addSubview(redview)
-//        redview.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
         
-        guard let cell = collectionView.cellForItem(at: indexPath) else {return}
-        guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else {return}
-        redview.frame = startingFrame
+        let red = UIViewController()
+        red.view.backgroundColor = .red
+        
+        let appFullscreenController = red
+        let redView = appFullscreenController.view!
+        redView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleRemoveRedView)))
+        view.addSubview(redView)
+
+        addChild(appFullscreenController)
+        
+        self.appFullscreenController = appFullscreenController
+        
+        guard let cell = collectionView.cellForItem(at: indexPath) else { return }
+        
+        guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else { return }
+        
         self.startingFrame = startingFrame
-        redview.layer.cornerRadius = 16
+        redView.frame = startingFrame
+        redView.layer.cornerRadius = 16
+        
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
-            redview.frame = self.view.frame
+            
+            redView.frame = self.view.frame
+            
+            self.tabBarController?.tabBar.frame.origin.y = self.view.frame.size.height
+            
         }, completion: nil)
     }
     
     var startingFrame: CGRect?
     
-    
-    @objc func handleRemoveRedView(gesture: UITapGestureRecognizer){
-        
+    @objc func handleRemoveRedView(gesture: UITapGestureRecognizer) {
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
+            
             gesture.view?.frame = self.startingFrame ?? .zero
+            
+            if let tabBarFrame = self.tabBarController?.tabBar.frame {
+                self.tabBarController?.tabBar.frame.origin.y = self.view.frame.size.height - tabBarFrame.height
+            }
+            
         }, completion: { _ in
             gesture.view?.removeFromSuperview()
+            self.appFullscreenController.removeFromParent()
         })
-        
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 7
+        return 4
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! TodayCell
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width - 48, height: 450)
+        return .init(width: view.frame.width - 64, height: 450)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -71,6 +89,7 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return .init(top: 32, left: 0, bottom: 0, right: 0)
+        return .init(top: 32, left: 0, bottom: 32, right: 0)
     }
+    
 }
